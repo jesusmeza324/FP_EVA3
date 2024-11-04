c830fa960a45e4e5dc0d1e6db05230e8bddae476
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2024-11-04 16:51:44 -0600

    Add files via upload

---
+++ EVA3_1_METODOS/src/main/java/com/mycompany/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_metodos;
+
+/**
+ *
+ * @author jesus
+ */
+public class EVA3_1_METODOS {
+
+    public static void main(String[] args) {
+        //invocar el metodo, llamar al metodo-->usarlo
+        imprimirMensaje("Hola mundo!!");
+    }
+    //declaracion del metodo
+    //modificadores
+           //valor retorno
+                      //nombre
+                               //parametros opcionales
+    //reutilizar codigo
+    public static void imprimirMensaje(String mensaje){
+        //cuerpo del metodo
+        System.out.println("tu mensaje:"+mensaje);
+    
+    }
+}
